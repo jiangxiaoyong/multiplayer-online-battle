588f5f3900d744c6f766acf070b8f987ec7adb29
xiaoyong jiang <jiangxiaoyong904@gmail.com>
2016-10-23 22:45:16 -0400

    fix input box of landing page

--- src_cljs/multiplayer_online_battle/landing.cljs
+++ src_cljs/multiplayer_online_battle/landing.cljs
@@ -9,39 +9,38 @@
 
 (enable-console-print!)
 
-(def input-val (r/atom ""))
-
-
 (defn register-user-info [ch-out component-attr]
-  [:div.container {:class (get-in @component-attr [:landing-pg :animate]) 
-                   :style {:display (get-in @component-attr [:landing-pg :visibility])}}
-   [:div.row
-    [:div.col-md-6.col-md-offset-3
-     [:div.panel.panel-login
-      [:div.panel-body
+  (let [input-val (r/atom "")]
+    (fn []
+      [:div.container {:class (get-in @component-attr [:landing-pg :animate]) 
+                       :style {:display (get-in @component-attr [:landing-pg :visibility])}}
        [:div.row
-        [:div.col-lg-12
-         [:form#register-form
-          [:h2
-           [:center "Multiple Online Battle Arena"]]
-          [:div.form-group
-           [:input {:type "text" 
-                    :id "username" 
-                    :class "form-control" 
-                    :placeholder "Username" 
-                    :value @input-val
-                    :on-change #(reset! input-val (-> % .-target .-value))}]]
-          [:div.form-group
+        [:div.col-md-6.col-md-offset-3
+         [:div.panel.panel-login
+          [:div.panel-body
            [:div.row
-            [:div.col-sm-6.col-sm-offset-3
-             [:input {:name "register-submit"
-                      :id "register-submit"
-                      :class "form-control btn btn-register"
-                      :value "Confirm"
-                      :disabled (if (str/blank? @input-val)
-                                  true
-                                  false)
-                      :on-click #(do
-                                   (go
-                                     (>! ch-out [:register-user/username {:username @input-val}]))
-                                   (swap! component-attr assoc-in [:landing-pg :animate] "animated fadeOutDown"))}]]]]]]]]]]]])
+            [:div.col-lg-12
+             [:form#register-form
+              [:h2
+               [:center "Multiple Online Battle Arena"]]
+              [:div.form-group
+               [:input {:type "text" 
+                        :id "username" 
+                        :class "form-control" 
+                        :placeholder "Username" 
+                        :value @input-val
+                        :on-change #(reset! input-val (-> % .-target .-value))}]]
+              [:div.form-group
+               [:div.row
+                [:div.col-sm-6.col-sm-offset-3
+                 [:input {:name "register-submit"
+                          :id "register-submit"
+                          :class "form-control btn btn-register"
+                          :value "Confirm"
+                          :disabled (if (str/blank? @input-val)
+                                      true
+                                      false)
+                          :on-click #(do
+                                       (go
+                                         (>! ch-out [:register-user/username {:username @input-val}]))
+                                       (swap! component-attr assoc-in [:landing-pg :animate] "animated fadeOutDown"))}]]]]]]]]]]]])))
